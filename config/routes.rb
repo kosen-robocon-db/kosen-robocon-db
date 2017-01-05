e9f6c20a4ca47e4d9814930f0cada3025d02695c
matthew238 <matthew.szulik@gmail.com>
2017-01-05 14:39:36 +0900

    add:参加統計ページを追加
    大会に参加した出場校・キャンパス、チーム数を表示する参加統計ページのコントローラーとビューを加えました。
    大会エントリー数のモデルは既に追加していましたが、参加統計のモデルとして読み替えてください。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
 
   resources :campuses, only: [:index, :show]
   resources :contests, only: [:index, :show]
+  resources :contest_entries, only: [:index]
 
 end
