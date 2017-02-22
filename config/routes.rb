3b7f480461066ca7504b9269727991dc8eb8176a
matthew238 <matthew.szulik@gmail.com>
2017-02-22 11:42:23 +0900

    add: robotコントローラーを追加
    ロボットコード（robotテーブル主キー）をURLに付加して"FQDN/robots/ロボットコード"でアクセスするとそのロボットコードを持つロボットの情報が表示される。
    ビュー(show.html.erb)なども追加。
    また、特定のキャンパス表示画面、特定の大会表示画面にロボット表示へのリンクを追加。

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :campuses, only: [:index, :show], param: :code
   resources :contests, only: [:index, :show], param: :nth
   resources :contest_entries, only: [:index]
+  resources :robots, only: [:show], param: :code
 
 end
