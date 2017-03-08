b116ec854ff63f024eb60d7137d6a9f5d8e53304
matthew238 <matthew.szulik@gmail.com>
2017-03-09 08:53:21 +0900

    del: db/seeds.rbで読み込み順を決定する方式を採用
    ＤＢへの初期データ登録方式を二つ考案したが、db/seeds.rbのload()の実行順で初期データ登録する方式を採用した。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
+# ファイル名の先頭で読み込み順を与える場合には
+# db/seedsディレクトリ中のファイルをグロービングさせるコードを利用。
+# ファイル例：01_region.rb, 02_campus.rb, 03_contest.rb, ...
 # Dir.glob(File.join(Rails.root, 'db', 'seeds', '*.rb')) do |file|
 #  load(file)
 # end
 
+# load()の実行順でデータベース初期データを与える場合には以下のコードを利用
 load(File.join(Rails.root, 'db', 'seeds', 'region.rb'))
 load(File.join(Rails.root, 'db', 'seeds', 'campus.rb'))
 load(File.join(Rails.root, 'db', 'seeds', 'contest.rb'))
