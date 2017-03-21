8562153d94717b8f09fa7265ca9edd6ece176b92
matthew238 <matthew.szulik@gmail.com>
2017-03-22 06:40:53 +0900

    fix: タイポ
    campus_controllerで@prizeを得るのにincludesのスペルをミスしていた。修正。

--- app/controllers/campuses_controller.rb
+++ app/controllers/campuses_controller.rb
@@ -7,6 +7,6 @@ class CampusesController < ApplicationController
     @campus = Campus.find_by(code: params[:code])
     @robots = @campus.robots.includes(:contest).order_default.on_page(params[:page])
     @campus_histories = CampusHistory.where(campus_code: params[:code])
-    @prizes = Prize.includes(:contest).where(campus_code: params[:code]).inclues(:prize_type)
+    @prizes = Prize.includes(:contest).where(campus_code: params[:code]).includes(:prize_type)
   end
 end
