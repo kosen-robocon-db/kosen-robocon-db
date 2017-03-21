f103223605f375e40c24f975a7a9a326885d612e
matthew238 <matthew.szulik@gmail.com>
2017-03-21 19:58:54 +0900

    add: pirze（賞）モデルの追加

--- app/models/prize.rb
+++ app/models/prize.rb
@@ -1,2 +1,13 @@
 class Prize < ApplicationRecord
+  belongs_to :contest,    foreign_key: :contest_nth, primary_key: :nth
+  belongs_to :region,     foreign_key: :region_code, primary_key: :code
+  belongs_to :campus,     foreign_key: :campus_code, primary_key: :code
+  belongs_to :robot,      foreign_key: :robot_code,  primary_key: :code
+  belongs_to :prize_type, foreign_key: :prize_type,  primary_key: :type
+
+  validates :contest_nth, presence: true
+  validates :region_code, presence: true
+  validates :campus_code, presence: true
+  validates :robot_code,  presence: true
+  validates :prize_type,  presence: true
 end
