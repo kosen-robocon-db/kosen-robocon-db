3222698772996c86a62c0b0553218cb6f9f75785
matthew238 <matthew.szulik@gmail.com>
2017-04-22 19:45:29 +0900

    add: 試合詳細の表示情報を追加
    試合のスコアや、審査員判定にもつれ込んだ場合の表示機能を追加。ただし、第29回大会用のみ。

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,16 @@
 class GamesController < ApplicationController
   def show
     @game = Game.find_by(code: params[:code])
+    region = Region.find_by(code: @game.region_code)
+    @title_for_tab =
+      @game.contest_nth.ordinalize + " " +
+      region.name[0, 1] + " " +
+      @game.round.to_s + "回戦" +
+      "第" + @game.game.to_s + "試合"
+    @title =
+      "第" + @game.contest_nth.to_s + "回 " +
+      region.name + "#{ @game.region_code >= 1 ? "地区" : "" }大会 " +
+      @game.round.to_s + "回戦 " +
+      "第" + @game.game.to_s + "試合"
   end
 end
