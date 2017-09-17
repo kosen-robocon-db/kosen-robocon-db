a8d1bc1746dbb9f8b864f3d2f793c8cbcb3697b1
matthew238 <matthew.szulik@gmail.com>
2017-09-17 15:19:18 +0900

    fix: db:seed:game_detailのJSON形式情報の余計な文字列を排除
    String#gsubと正規表現を使って改行文字、空白文字を削除。但しこの方法では項目内の空白文字も削除されるので、よりよい正規表現または別の方法で余計な文字列を与えないようにすべき。

--- db/seeds/game_detail.rb
+++ db/seeds/game_detail.rb
@@ -7,15 +7,15 @@
 # Game.create(code: 1291202, contest_nth: 29, region_code: 1, round: 2, game: 2, left_robot_code: 129110101, right_robot_code: 129110501, winner_robot_code: 129110101)
 # Game.create(code: 1291301, contest_nth: 29, region_code: 1, round: 3, game: 1, left_robot_code: 129110201, right_robot_code: 129110101, winner_robot_code: 129110101)
 bulk_insert_data = []
-bulk_insert_data << GameDetail.new(game_code: 1291101, properties: '{
+bulk_insert_data << GameDetail.new(game_code: 1291101, number: 1, properties: '{
   "score":"0-0",
   "judge":"3-0",
   "progress":"灯台-なし"
-}')
-bulk_insert_data << GameDetail.new(game_code: 1291102, properties: '{
+}'.gsub(/\n| /, ""))
+bulk_insert_data << GameDetail.new(game_code: 1291102, number: 1, properties: '{
   "score":"0-0",
   "judge":"2-1",
   "progress":"灯台-灯台",
   "memo":"釧路は旭川より灯台完成が早かった。"
-}')
+}'.gsub(/\n| /, ""))
 GameDetail.import bulk_insert_data
