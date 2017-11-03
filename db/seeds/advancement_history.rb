e652521bd0481787dcfe8ce197f5725705645ebb
matthew238 <matthew.szulik@gmail.com>
2017-11-03 17:58:10 +0900

    fix: 全国大会進出情報初期データの修正
    大会回が全て間違っていたので修正。

--- db/seeds/advancement_history.rb
+++ db/seeds/advancement_history.rb
@@ -19,7 +19,7 @@ else
   # 神戸高専
   # 乳母車
   bulk_insert_data << AdvancementHistory.new(
-    contest_nth: 8, region_code: 5, campus_code: 5370, robot_code: 107553701,
+    contest_nth: 7, region_code: 5, campus_code: 5370, robot_code: 107553701,
     advancement_case: 1, decline: false, memo: ""
   )
 
@@ -31,37 +31,37 @@ else
 
   # 白龍
   bulk_insert_data << AdvancementHistory.new(
-    contest_nth: 8, region_code: 5, campus_code: 5370, robot_code: 109553701,
+    contest_nth: 9, region_code: 5, campus_code: 5370, robot_code: 109553701,
     advancement_case: 2, decline: false, memo: ""
   )
 
   # ちゃれんじろう
   bulk_insert_data << AdvancementHistory.new(
-    contest_nth: 8, region_code: 5, campus_code: 5370, robot_code: 112553701,
+    contest_nth: 12, region_code: 5, campus_code: 5370, robot_code: 112553701,
     advancement_case: 1, decline: false, memo: ""
   )
 
   # 栗太郎
   bulk_insert_data << AdvancementHistory.new(
-    contest_nth: 8, region_code: 5, campus_code: 5370, robot_code: 115553701,
+    contest_nth: 15, region_code: 5, campus_code: 5370, robot_code: 115553701,
     advancement_case: 1, decline: false, memo: ""
   )
 
   # 撃ま栗
   bulk_insert_data << AdvancementHistory.new(
-    contest_nth: 8, region_code: 5, campus_code: 5370, robot_code: 116553702,
+    contest_nth: 16, region_code: 5, campus_code: 5370, robot_code: 116553702,
     advancement_case: 2, decline: false, memo: ""
   )
 
   # 甲速［ｍ／ｓ］
   bulk_insert_data << AdvancementHistory.new(
-    contest_nth: 8, region_code: 5, campus_code: 5370, robot_code: 127553701,
+    contest_nth: 27, region_code: 5, campus_code: 5370, robot_code: 127553701,
     advancement_case: 1, decline: false, memo: ""
   )
 
   # さるかにんぼう
   bulk_insert_data << AdvancementHistory.new(
-    contest_nth: 8, region_code: 5, campus_code: 5370, robot_code: 130553701,
+    contest_nth: 30, region_code: 5, campus_code: 5370, robot_code: 130553701,
     advancement_case: 2, decline: false, memo: ""
   )
 end
