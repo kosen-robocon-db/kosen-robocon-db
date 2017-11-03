52d3359ec0872fca31edc79be3d5fa8e27fff800
matthew238 <matthew.szulik@gmail.com>
2017-11-03 17:57:27 +0900

    add: 全国大会進出情報一覧をキャンパス毎のページに追加

--- app/models/advancement_history.rb
+++ app/models/advancement_history.rb
@@ -1,9 +1,19 @@
 class AdvancementHistory < ApplicationRecord
+  belongs_to :contest,     foreign_key: :contest_nth,      primary_key: :nth
+  belongs_to :region,      foreign_key: :region_code,      primary_key: :code
+  belongs_to :campus,      foreign_key: :campus_code,      primary_key: :code
   belongs_to :robot,       foreign_key: :robot_code,       primary_key: :code
   belongs_to :advancement, foreign_key: :advancement_case, primary_key: :case
 
   validates :robot_code,        presence: true #, uniqueness: true
   # validates :decline,           inclusion: { in: [true, false] }
+
+  scope :order_default, -> { order(
+    contest_nth: :asc,
+    region_code: :asc,
+    campus_code: :asc,
+    robot_code: :asc,
+  ) }
 
   def self.csv_headers
     # UTF-8出力される
