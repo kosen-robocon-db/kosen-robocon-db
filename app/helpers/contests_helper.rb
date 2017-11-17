7e4555a0560f6b862cd52904b71db69f6cf76fe5
matthew238 <matthew.szulik@gmail.com>
2017-11-17 11:58:02 +0900

    add: 前後大会へのリンクの追加　その２
    前後大会へのリンクのうち、各コンテストの表示ページに前後大会へのリンクを追加した。受賞結果ページと同様の実装の方法です。contest_helperに前後大会への遷移制御が実装されています。

--- app/helpers/contests_helper.rb
+++ app/helpers/contests_helper.rb
@@ -1,2 +1,21 @@
 module ContestsHelper
+
+  def link_to_prior_contest(contest_nth)
+    if contest_nth.to_i > 1 then # It's a hard code! But never changed.
+      link_to fa_icon("chevron-left", text: "前回"),
+        contest_path(
+          nth: (contest_nth.to_i - 1).to_s
+        )
+    end
+  end
+
+  def link_to_posterior_contest(contest_nth)
+    if contest_nth.to_i < 30 then # It's a hard code! Propose alternative ways!
+      link_to fa_icon("chevron-right", text: "次回"),
+        contest_path(
+          nth: (contest_nth.to_i + 1).to_s
+        )
+    end
+  end
+
 end
--- app/helpers/prize_histories_helper.rb
+++ app/helpers/prize_histories_helper.rb
@@ -1,6 +1,6 @@
 module PrizeHistoriesHelper
 
-  def link_to_prior_page(contest_nth, region_code)
+  def link_to_prior_prize_history(contest_nth, region_code)
     if contest_nth.to_i > 4 then # It's a hard code! But never changed.
       link_to fa_icon("chevron-left", text: "前回"),
         contest_prize_history_path(
@@ -10,7 +10,7 @@ module PrizeHistoriesHelper
     end
   end
 
-  def link_to_posterior_page(contest_nth, region_code)
+  def link_to_posterior_prize_history(contest_nth, region_code)
     if contest_nth.to_i < 30 then # It's a hard code! Propose alternative ways!
       link_to fa_icon("chevron-right", text: "次回"),
         contest_prize_history_path(
