413dd775d3a29e74982dcf530283353edc7ca2b7
matthew238 <matthew.szulik@gmail.com>
2017-11-24 21:46:19 +0900

    update: ロボット情報のページ(showアクション)で準決勝や決勝を表示
    これまで4回戦や5回戦と表示していたところを準決勝や決勝と表示する改善変更を行った。

--- app/helpers/robots_helper.rb
+++ app/helpers/robots_helper.rb
@@ -1,19 +1,11 @@
 module RobotsHelper
   # ノックアウト方式だけではない予選がある大会もあるのでこのcaseで分類する方式を採用
-  def game_name(nth:, region:,round:, game:)
-    logger.debug(">>>> RobotsHelper#game_name #{nth}, #{region}, #{round}, #{game}")
-    a = "#{round}回戦 第#{game}試合"
-    case nth
-    when 29 then # 第２９回大会
-      case region
-      when 8 then # 九州沖縄
-        case round
-        when 5 then # 準決勝
-          a = "準決勝 第#{game}試合"
-        when 6 then # 決勝
-          a = "決勝戦"
-        end
-      end
+  def game_name(nth:, region:, round:, game:)
+    round_name = RoundName.find_by(contest_nth: nth, region_code: region, round: round)
+    logger.debug(">>>> RobotsHelper#game_name #{nth}, #{region}, #{round}, #{game}, #{round_name.name}")
+    a = "#{round_name.name}"
+    if round_name.name != "決勝" then
+      a += " 第#{game}試合"
     end
     a
   end
