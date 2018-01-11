d09d0f797b1be7ac27aee6aa2c1dbd5b31bab6c9
matthew238 <matthew.szulik@gmail.com>
2018-01-11 13:36:43 +0900

    improve: 対戦情報エラーメッセージを改善
    同じ試合コードの情報を登録しようとすると、これまでは属性の変数名が表示されていたが、それを日本語で表示する改善を行った。また、モデルとビューで属性名やエラーメッセージなどを分けることにした。

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module RoboconDb
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    p config.i18n.load_path
     config.i18n.default_locale = :ja
 
     config.autoload_paths << Rails.root.join("app/models/game_details")
