3e65592e0044690067946f6d7f77284be0d6f769
Ken ichi AKIMOTO <Matthew238@users.noreply.github.com>
2018-01-29 09:30:20 +0900

    fix: #115とoverall-improvementの整合性のためのコミット

--- app/models/campus.rb
+++ app/models/campus.rb
@@ -4,7 +4,7 @@ class Campus < ApplicationRecord
       code: 0, region_code: 0, name: "キャンパスなし", abbreviation: "キャンパスなし"
     )
   end
-  
+
   belongs_to :region, foreign_key: :region_code, primary_key: :code
   has_many :robots, foreign_key: :campus_code, primary_key: :code, dependent: :destroy
   has_many :campus_history, foreign_key: :campus_code, primary_key: :code, dependent: :destroy
