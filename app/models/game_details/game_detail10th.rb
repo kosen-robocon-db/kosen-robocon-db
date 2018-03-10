33b0a39dca7d1364745c7ed1df674f40b2aabc35
matthew238 <matthew.szulik@gmail.com>
2018-03-10 21:02:16 +0900

    add: 第10回大会対戦情報の試合詳細を追加

--- app/models/game_details/game_detail10th.rb
+++ app/models/game_details/game_detail10th.rb
@@ -1,2 +1,76 @@
 class GameDetail10th < GameDetail
+
+  # my_robot_code側から見ているので、
+  # ロボットコード異なる場合は交換したい左右の値の語幹を書いておく
+  ROOTS = %w( robot_code gaining_point art_point total_point )
+
+  REX_PT   = /[0-9]|1[0-8]|#{GameDetail::Constant::UNKNOWN_VALUE}/
+  REX_APT  = /[0-9]|#{GameDetail::Constant::UNKNOWN_VALUE}/
+  REX_TPT  = /[0-9]|1[0-9]|2[0-7]|#{GameDetail::Constant::UNKNOWN_VALUE}/
+
+  attr_accessor :my_gaining_point, :opponent_gaining_point
+  attr_accessor :my_art_point, :opponent_art_point
+  attr_accessor :my_total_point, :opponent_total_point
+  attr_accessor :extra_time # 再々延長ルールはあったが適用される試合はなかったはず
+  attr_accessor :memo
+
+  # validates に numericality: {} を指定したいところだが(分かり易いが)、
+  # format: {} のほうが手間がかからないようなので、採用した。
+  validates :my_gaining_point, format: { with: REX_PT }
+  validates :opponent_gaining_point, format: { with: REX_PT }
+  validates :my_art_point, format: { with: REX_APT }
+  validates :opponent_art_point, format: { with: REX_APT }
+  validates :my_total_point, format: { with: REX_TPT }
+  validates :opponent_total_point, format: { with: REX_TPT }
+  validates :memo, length: { maximum: 255 }
+
+  # DBにはないがpropertyに納めたいフォーム上の属性
+  def self.additional_attr_symbols
+    [
+      :my_gaining_point, :opponent_gaining_point,
+      :my_art_point, :opponent_art_point,
+      :my_total_point, :opponent_total_point,
+      :extra_time,
+      :memo
+    ]
+  end
+
+  def roots
+    ROOTS
+  end
+
+  # SRP(Single Responsibility Principle, 単一責任原則)に従っていないが
+  # このクラス内で実装する。
+  def self.compose_properties(hash:)
+    h = super(hash: hash) || {}
+    ROOTS.each do |pr|
+      my_sym, opponent_sym = "my_#{pr}".to_sym, "opponent_#{pr}".to_sym
+      if hash[my_sym].present? and hash[opponent_sym].present?
+        h["#{pr}"] = "#{hash[my_sym]}#{DELIMITER}#{hash[opponent_sym]}"
+      end
+    end
+    h["extra_time"] = "true" if hash[:extra_time].present?
+    h["memo"] = "#{hash[:memo]}" if hash[:memo].present?
+    return h
+  end
+
+  def decompose_properties(robot:)
+    super(robot: robot) do |h|
+      if h["gaining_point"].present?
+        self.my_gaining_point, self.opponent_gaining_point =
+          h["gaining_point"].to_s.split(DELIMITER)
+      end
+      if h["art_point"].present?
+        self.my_art_point, self.opponent_art_point =
+          h["art_point"].to_s.split(DELIMITER)
+      end
+      if h["total_point"].present?
+        self.my_total_point, self.opponent_total_point =
+          h["total_point"].to_s.split(DELIMITER)
+      end
+      self.extra_time = h["extra_time"].present? ? true : false
+      self.memo = h["memo"].presence || ''
+    end
+  end
+
 end
