94a8f692b74725eb9abfb91e491be047cfcba1ce
matthew238 <matthew.szulik@gmail.com>
2018-03-16 16:26:46 +0900

    update: 得点入力フォームなどのselect option用の配列準備ヘルパーメソッドを追加してリファクタリング
    試合詳細のビューの先頭で得点などのselect
    option用に定義している配列をヘルパーメソッド化してより簡略化した記述に変更し、第1-10回大会までリファクタリングを行った。

--- app/helpers/game_details_helper.rb
+++ app/helpers/game_details_helper.rb
@@ -1,2 +1,32 @@
 module GameDetailsHelper
+  def option_numbers(
+    default_display:     "--",
+    default_value:      GameDetail::Constant::UNKNOWN_VALUE,
+    begin_number:           0,
+    end_number:            59,
+    digit:                  2,
+    padding_string:       ' ',
+    value_padding:      false
+  )
+    numbers = Array([[default_display, default_value]])
+    if value_padding then
+      numbers.concat(Array.new(end_number - begin_number + 1) { |i|
+        [
+          ( i + begin_number ).to_s.rjust(digit, padding_string),
+          ( i + begin_number ).to_s.rjust(digit, padding_string)
+        ]
+      })
+    else
+      numbers.concat(Array.new(end_number - begin_number + 1) { |i|
+        [
+          ( i + begin_number ).to_s.rjust(digit, padding_string),
+          i + begin_number
+        ]
+      })
+    end
+  end
+
+  def time_numbers
+    option_numbers padding_string: '0', value_padding: true
+  end
 end
