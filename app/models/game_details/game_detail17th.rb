7854705d63ad8535d0f79998bb3a06effb3f113c
matthew238 <matthew.szulik@gmail.com>
2018-04-04 21:18:52 +0900

    update: コミット漏れ

--- app/models/game_details/game_detail17th.rb
+++ app/models/game_details/game_detail17th.rb
@@ -1,16 +1,23 @@
 class GameDetail17th < GameDetail
-  # メッセンジャーボールの距離が同じであれば審査員判定（地区3名、全国？名）
-  # 反則1回につき50cm距離追加
-  # 距離が短い方が勝ち
+  # 反則
+  #   反則1回につき50cm距離追加
+  # 勝敗
+  #   距離が短い方が勝ち
+  #   計測は0.5cm刻み
+  #   同じ距離の場合は審査員判定
+  # 審査員判定
+  # 　メッセンジャーボールの距離が同じであれば審査員判定（地区3名、全国？名）
 
   # my_robot_code側から見ているので、
   # ロボットコード異なる場合は交換したい左右の値の語幹を書いておく
   STEMS = %w( robot_code gaining_point deducting_point total_point retry
     jury_votes )
 
-  REX_DPT = /[0-9]|#{GameDetail::Constant::UNKNOWN_VALUE}/
-  REX_RT  = /[0-1]|#{GameDetail::Constant::UNKNOWN_VALUE}/
-  REX_VT  = /[0-5]|#{GameDetail::Constant::UNKNOWN_VALUE}/
+  REX_GPT = /\A(([1-9]\d{,2}|0)(\.5+)?|)\z/ # 何も入力しないを許す。
+  REX_DPT = /\A([0-9]|#{UNKNOWN})\z/
+  REX_TPT = /\A(([1-9]\d{,2}|0)(\.5+)?|)\z/ # 何も入力しないを許す。負なしと仮定。
+  REX_RT  = /\A([0-1]|#{UNKNOWN})\z/
+  REX_VT  = /\A([0-5]|#{UNKNOWN})\z/
 
   attr_accessor :my_gaining_point,   :opponent_gaining_point   # 距離の代わり
   attr_accessor :my_deducting_point, :opponent_deducting_point # 反則数の代わり
@@ -20,16 +27,12 @@ class GameDetail17th < GameDetail
   attr_accessor :my_jury_votes,      :opponent_jury_votes
   attr_accessor :memo
 
-  # マイナスの記録はあったのだろうか？
-  # 不明の記録を入力できるようにするにはどうしたらよいのか？
-  validates :my_gaining_point, numericality: {
-    greater_than_or_equal_to: 0, less_than_or_equal_to: 300
-  }
-  validates :opponent_gaining_point, numericality: {
-    greater_than_or_equal_to: 0, less_than_or_equal_to: 300
-  }
+  validates :my_gaining_point,         format: { with: REX_GPT }
+  validates :opponent_gaining_point,   format: { with: REX_GPT }
   validates :my_deducting_point,       format: { with: REX_DPT }
   validates :opponent_deducting_point, format: { with: REX_DPT }
+  validates :my_total_point,           format: { with: REX_TPT }
+  validates :opponent_total_point,     format: { with: REX_TPT }
   validates :my_retry,                 format: { with: REX_RT }
   validates :opponent_retry,           format: { with: REX_RT }
   with_options if: :jury_votes do
@@ -51,12 +54,81 @@ class GameDetail17th < GameDetail
     ]
   end
 
+  # 親クラスから子クラスのSTEM定数を参照するためのメソッド
   def stems
     STEMS
   end
 
   def self.compose_properties(hash:)
-    h = compose_pairs(hash: hash, stems: STEMS)
+    h = super(hash: hash) || {}
+    # compose_pairsで拾えないgaining_point(入力なし)に対応
+    if
+      hash[:my_gaining_point].present? and
+      hash[:opponent_gaining_point].present?
+    then
+      h["gaining_point"] = "\
+        #{hash[:my_gaining_point]}\
+        #{DELIMITER}\
+        #{hash[:opponent_gaining_point]}\
+      ".gsub(/(\s| )+/, '')
+    end
+    if
+      hash[:my_gaining_point].present? and
+      hash[:opponent_gaining_point].blank?
+    then
+      h["gaining_point"] = "\
+        #{hash[:my_gaining_point]}\
+        #{DELIMITER}\
+        #{UNKNOWN}\
+      ".gsub(/(\s| )+/, '')
+    end
+    if
+      hash[:my_gaining_point].blank? and
+      hash[:opponent_gaining_point].present?
+    then
+      h["gaining_point"] = "\
+        #{UNKNOWN}\
+        #{DELIMITER}\
+        #{hash[:opponent_gaining_point]}\
+      ".gsub(/(\s| )+/, '')
+    end
+    h.update(compose_pairs(hash: hash, stems: %w( deducting_point )))
+    # compose_pairsで拾えないtotal_point(入力なし)に対応
+    if
+      hash[:my_total_point].present? and
+      hash[:opponent_total_point].present?
+    then
+      h["total_point"] = "\
+        #{hash[:my_total_point]}\
+        #{DELIMITER}\
+        #{hash[:opponent_total_point]}\
+      ".gsub(/(\s| )+/, '')
+    end
+    if
+      hash[:my_total_point].present? and
+      hash[:opponent_total_point].blank?
+    then
+      h["total_point"] = "\
+        #{hash[:my_total_point]}\
+        #{DELIMITER}\
+        #{UNKNOWN}\
+      ".gsub(/(\s| )+/, '')
+    end
+    if
+      hash[:my_total_point].blank? and
+      hash[:opponent_total_point].present?
+    then
+      h["total_point"] = "\
+        #{UNKNOWN}\
+        #{DELIMITER}\
+        #{hash[:opponent_total_point]}\
+      ".gsub(/(\s| )+/, '')
+    end
+    h.update(compose_pairs(hash: hash, stems: %w( retry jury_votes )))
+    if hash[:special_win].presence.to_bool
+      h["special_win"] = "true"
+      h.update(compose_time(hash: hash)) # h["time"]
+    end
     h.delete("jury_votes") unless hash["jury_votes"].presence.to_bool
     h["memo"] = "#{hash[:memo]}" if hash[:memo].present?
     return h
@@ -66,15 +138,23 @@ class GameDetail17th < GameDetail
     super(robot: robot) do |h|
       if h["gaining_point"].present?
         self.my_gaining_point, self.opponent_gaining_point =
-          h["gaining_point"].to_s.split(REX_SC)[1..-1]
+          h["gaining_point"].to_s.split(DELIMITER)
+        self.my_gaining_point =
+          "" if self.my_gaining_point == "#{UNKNOWN}"
+        self.opponent_gaining_point =
+          "" if self.opponent_gaining_point == "#{UNKNOWN}"
       end
       if h["deducting_point"].present?
         self.my_deducting_point, self.opponent_deducting_point =
-          h["deducting_point"].to_s.split(REX_SC)[1..-1]
+          h["deducting_point"].to_s.split(DELIMITER)
       end
       if h["total_point"].present?
         self.my_total_point, self.opponent_total_point =
-          h["total_point"].to_s.split(REX_SC)[1..-1]
+          h["total_point"].to_s.split(DELIMITER)
+        self.my_total_point =
+          "" if self.my_total_point == "#{UNKNOWN}"
+        self.opponent_total_point =
+          "" if self.opponent_total_point == "#{UNKNOWN}"
       end
       self.my_retry, self.opponent_retry =
         h["retry"].to_s.split(DELIMITER) if h["retry"].present?
