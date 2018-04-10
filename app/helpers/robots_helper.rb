8c7c913d0a46406ab70c30f5c2b0d83ee6857e95
matthew238 <matthew.szulik@gmail.com>
2018-04-10 20:08:08 +0900

    change: 試合名を返すヘルパー関数の引数をハッシュに変更
    robots_helperに定義していたgame_nameメソッドの引数は変更前まで4つだったが、これからはその四つを含んだハッシュを引数として与えることにした。この変更により、Gameオブジェクトを与えるだけでよくなった。

--- app/helpers/robots_helper.rb
+++ app/helpers/robots_helper.rb
@@ -1,18 +1,18 @@
 module RobotsHelper
   # ノックアウト方式だけではない予選がある大会もあるのでこのcaseで分類する方式を採用
-  def game_name(nth:, region:, round:, game:)
+  def game_name(hash:)
     round_name =
       RoundName.find_by(
-        contest_nth: nth,
-        region_code: region,
-        round: round
+        contest_nth: game["contest_nth"],
+        region_code: game["region_code"],
+        round: game["round"]
       ) ||
       RoundName.new(
         name: "未定義"
       )
     a = "#{round_name.name}"
     if round_name.name != "決勝" then
-      a += " 第#{game}試合"
+      a += " 第#{game["game"]}試合"
     end
     a
   end
