30877c204a8415a950aaf30e8f3d1dad0d21f952
matthew238 <matthew.szulik@gmail.com>
2018-04-11 16:45:43 +0900

    fix: メッセージ変更

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -4,7 +4,7 @@ descrite Game do
   # 勝利ロボットコードが勝利者なしコードで、
   # 対戦相手ロボットコードが対戦者なしコードの場合
   # 単独試合
-  it 'is valid if winner_robot_code is no winner code and opponent robot code\
+  it 'is solo if winner_robot_code is no winner code and opponent robot code\
     is no opponent code'
 
 end
