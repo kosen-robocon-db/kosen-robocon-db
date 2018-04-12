ac333ffc89a38f5deffc5747ed4a7890e3302fda
matthew238 <matthew.szulik@gmail.com>
2018-04-12 17:50:14 +0900

    change: gameの定数の幾つかをenumに変更

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,35 +8,38 @@ class Game < ApplicationRecord
       code: 100000002, contest_nth: 0, campus_code: 0,
       name: "勝者なし", kana: "ショウシャナシ"
     )
-    # Gameモデル／テーブルはleft_robot_code、right_robot_code、そして
-    # winner_robot_codeで勝敗を確定する。
-    # robot_code_1から見た場合：
-    # left_robot_code | right_robot_code | winner_robot_code |
-    # ----------------+------------------+-------------------+---------
-    # robot_code_1    | robot_code_2     | robot_code_1/2    | WIN/LOSE
-    # robot_code_1    | NO_OPPONENT      | NO_WINNER         | SOLO
-    # robot_code_1    | NO_OPPONENT      | robot_code_1      | BYE
-    # robot_code_1    | robot_code_2     | NO_WINNER         | BOTH_DSQ
-    # ※ NO_OPPONENT及びNO_WINNERは同じ値でも機能する？
-    WIN      = '1' # 勝ち
-    LOSE     = '2' # 負け
-    SOLO     = '3' # 単独競技
-    BYE      = '4' # 不戦勝
-    BOTH_DSQ = '5' # 両者失格
   end
 
+  # Gameモデル／テーブルはleft_robot_code、right_robot_code、そして
+  # winner_robot_codeで勝敗を確定する。
+  # robot_code_1から見た場合：
+  # left_robot_code | right_robot_code | winner_robot_code |
+  # ----------------+------------------+-------------------+---------
+  # robot_code_1    | robot_code_2     | robot_code_1/2    | win/lose
+  # robot_code_1    | NO_OPPONENT      | NO_WINNER         | solo
+  # robot_code_1    | NO_OPPONENT      | robot_code_1      | bye
+  # robot_code_1    | robot_code_2     | NO_WINNER         | both_dsq
+  # ※ NO_OPPONENT及びNO_WINNERは同じ値でも機能する？
+  enum result: {
+    win:      1, # 勝ち
+    lose:     2, # 負け
+    solo:     3, # 単独競技
+    bye:      4, # 不戦勝
+    both_dsq: 5, # 両者失格
+  }
+
   # 試合途中棄権をretireとすべきところを、
-  # 第1回はレースであった、そして度々レースがあったので、
-  # レースなどでよく使われているDNFにし、
+  # 第1第1回はレースであったし度々レースがあったので、
+  # レースに因んでレースなどでよく使われているDNFにし、
   # それに合わせて試合前棄権をDNS、失格をDSQとした。
   # 1,2,3,...などの数値の代わりに日本語テキスト表示をするために
   # enum_help(GEM)をインストールし（必須）、
   # kosen-robocon.ja.ymlに日本語テキストを記している。
-  enum reasons: {
-    draw: 1,      # 引き分け（審査員判定・推薦で勝ち負け）
-    DNS:  2,      # Do Not Start 負け側が試合開始前棄権
-    DNF:  3,      # Do Not Finish 負け側が試合途中棄権
-    DSQ:  4       # Disqualification 負け側が失格　
+  enum reason: {
+    draw: 1, # 引き分け（審査員判定・推薦で勝ち負け）
+    DNS:  2, # Do Not Start 負け側が試合開始前棄権
+    DNF:  3, # Do Not Finish 負け側が試合途中棄権
+    DSQ:  4  # Disqualification 負け側が失格　
   }
 
   before_validation :compose_attributes
@@ -56,11 +59,9 @@ class Game < ApplicationRecord
   validates :right_robot_code,  presence: true
   validates :winner_robot_code, presence: true
 
-  attr_accessor :robot_code, :opponent_robot_code, :victory
-  validates :victory, inclusion: { in: [
-    Constant::WIN, Constant::LOSE, Constant::SOLO, Constant::BYE,
-    Constant::BOTH_DSQ
-  ] }
+  attr_accessor :robot_code, :opponent_robot_code
+  attr_accessor :victory # victory or defeat / 勝敗の意味で使っている
+  validates :victory, inclusion: { in: results.each.map { |k,v| v.to_s } }
 
   scope :order_csv, -> { order(id: :asc) }
 
@@ -95,23 +96,43 @@ class Game < ApplicationRecord
       self.right_robot_code : self.left_robot_code
     # compose_attributesにある処理と逆処理なのでハッシュ関数化して
     # 両方とも簡単にできないだろうか？
+    # case self.winner_robot_code
+    # when Constant::NO_WINNER.code then
+    #   case self.opponent_robot_code
+    #   when Constant::NO_OPPONENT.code then
+    #     self.victory = Constant::SOLO
+    #   else
+    #     self.victory = Constant::BOTH_DSQ
+    #   end
+    # else
+    #   case self.opponent_robot_code
+    #   when Constant::NO_OPPONENT.code then
+    #     self.victory = Constant::BYE
+    #   else
+    #     if self.robot_code == self.winner_robot_code then
+    #       self.victory = Constant::WIN
+    #     else
+    #       self.victory = Constant::LOSE
+    #     end
+    #   end
+    # end
     case self.winner_robot_code
     when Constant::NO_WINNER.code then
       case self.opponent_robot_code
       when Constant::NO_OPPONENT.code then
-        self.victory = Constant::SOLO
+        self.victory = Game.results[:solo].to_s
       else
-        self.victory = Constant::BOTH_DSQ
+        self.victory = Game.results[:both_dsq].to_s
       end
     else
       case self.opponent_robot_code
       when Constant::NO_OPPONENT.code then
-        self.victory = Constant::BYE
+        self.victory = Game.results[:bye].to_s
       else
         if self.robot_code == self.winner_robot_code then
-          self.victory = Constant::WIN
+          self.victory = Game.results[:win].to_s
         else
-          self.victory = Constant::LOSE
+          self.victory = Game.results[:lose].to_s
         end
       end
     end
@@ -193,17 +214,29 @@ class Game < ApplicationRecord
     self.left_robot_code = self.robot_code
     self.right_robot_code = self.opponent_robot_code
     case self.victory
-    when Constant::WIN then
+    # when Constant::WIN then
+    #   self.winner_robot_code = self.robot_code
+    # when Constant::LOSE then
+    #   self.winner_robot_code = self.opponent_robot_code
+    # when Constant::SOLO then
+    #   self.right_robot_code  = Constant::NO_OPPONENT.code
+    #   self.winner_robot_code = Constant::NO_WINNER.code
+    # when Constant::BYE then
+    #   self.right_robot_code = Constant::NO_OPPONENT.code
+    #   self.winner_robot_code = self.robot_code
+    # when Constant::BOTH_DSQ then
+    #   self.winner_robot_code = Constant::NO_WINNER.code
+    when Game.results[:win].to_s then
       self.winner_robot_code = self.robot_code
-    when Constant::LOSE then
+    when Game.results[:lose].to_s then
       self.winner_robot_code = self.opponent_robot_code
-    when Constant::SOLO then
+    when Game.results[:solo].to_s then
       self.right_robot_code  = Constant::NO_OPPONENT.code
       self.winner_robot_code = Constant::NO_WINNER.code
-    when Constant::BYE then
+    when Game.results[:bye].to_s then
       self.right_robot_code = Constant::NO_OPPONENT.code
       self.winner_robot_code = self.robot_code
-    when Constant::BOTH_DSQ then
+    when Game.results[:both_dsq].to_s then
       self.winner_robot_code = Constant::NO_WINNER.code
     else
       self.winner_robot_code = Constant::NO_WINNER.code
