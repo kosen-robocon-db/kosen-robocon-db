c593c1fec3b2dceae568b3045c78572b6e890f2c
matthew238 <matthew.szulik@gmail.com>
2018-04-20 17:21:16 +0900

    improve: 戻るリンクを集約し、どの遷移元へも戻れることを可能にした
    サイト内の遷移元ページへ、さらに外部ページから直接遷移した場合でも、遷移元のページに戻れることを可能にした。
    戻るリンクはlayout/application.html.erbに集約した。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
 
+  before_filter :set_request_from
+
   def base_title
     "高専ロボコンデータベース"
   end
@@ -22,6 +24,24 @@ module ApplicationHelper
       type
     else
       "info"
+    end
+  end
+
+  # セッションのリクエスト元を保存しておく
+  def set_request_from
+    if session[:request_from]
+      @request_from = session[:request_from]
+    end
+    # 現在のURLを保存しておく
+    session[:request_from] = request.original_url
+  end
+
+  # 前の画面に戻る
+  def return_back
+    if request.referer
+      redirect_to :back and return true
+    elsif @request_from
+      redirect_to @request_from and return true
     end
   end
 
