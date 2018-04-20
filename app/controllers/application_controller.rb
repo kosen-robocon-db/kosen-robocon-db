1d599f66ed63c1c76f63b81a9610a7f2aaf5c21c
matthew238 <matthew.szulik@gmail.com>
2018-04-20 18:04:59 +0900

    fix: どこからでも戻れるリンクの機能実装場所間違いを修正
    ApplicationHelperではなくApplicationControllerクラスに書くべきであったので修正した。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_request_from
   protect_from_forgery with: :exception
+
+  # セッションのリクエスト元を保存しておく
+  def set_request_from
+    if session[:request_from]
+      @request_from = session[:request_from]
+    end
+    # 現在のURLを保存しておく
+    session[:request_from] = request.original_url
+  end
+
+  # 前の画面に戻る
+  def return_back
+    if request.referer
+      redirect_to :back and return true
+    elsif @request_from
+      redirect_to @request_from and return true
+    end
+  end
 
   private
     def logged_in_user
