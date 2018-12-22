0efae7fa75ba6074ce7bb9717e3bdfa6bc1e5431
matthew238 <matthew.szulik@gmail.com>
2018-12-22 21:22:51 +0900

    fix: loggerへのデバッグレベル指定を削除

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
-    config.logger.level = Logger::DEBUG
+    # config.logger.level = Logger::DEBUG
   end
 
   # Do not dump schema after migrations.
